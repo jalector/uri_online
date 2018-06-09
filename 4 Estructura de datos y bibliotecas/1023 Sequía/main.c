021af97eae13ef1d1e4bd1a97929bf327c84b2c7
jalector <judaalector@hotmail.com>
2018-06-09 09:24:08 -0500

    bajar

--- 4 Estructura de datos y bibliotecas/1023 Sequía/main.c
+++ 4 Estructura de datos y bibliotecas/1023 Sequía/main.c
@@ -6,63 +6,68 @@
 #include <math.h>
 
 void main(){
-	//Número de casos.
 	int cases = 0;	
-	//Número de ciudades.
 	int cities = 0;
 	do{
-		scanf("%i",&cases);
-		//Sí no es el caso de termino.
+		if(scanf("%i",&cases) == -1)break;
+		//Reviso sí es el caso final.
 		if(cases > 0){
-			//Imprimimos sí no es el primero.
 			if(cities > 0)printf("\n");
-			//Arreglo de información en forma de tabla(arreglo bidimencional).
+			//Estructura de datos bidimensional.
 			int data[cases][2];
-			//Agua y personas totales.
-			int water = 0, citizen = 0;
-			// Para ordenar el arreglo de información.
-			int max = 0; 
-			//Leer la inforamción
-			for (int i = 0; i < cases; i++){
-				scanf("%i %i", &data[i][0], &data[i][1]);
-				//Conteo total de la agua y las personas de la ciudad.
-				citizen += data[i][0];
-				water += data[i][1];
-			}
-
-			//Comienza bubble sort
-			for(int i = 0; i < cases; i++){
-				int top = cases-i;
-				for(int j = 1; j < top; j++){
-					int promedio1 = (int) floor(data[j][1]/data[j][0]),
-					    promedio2 = (int) floor(data[max][1]/data[max][0]);
-					//Encuetro la posición del mas grande
-					if(promedio1 > promedio2){						
-						max = j;
-					}
-					//Lo deposito en último lugar disponible.
-					if(j == top-1){
-						int aux1 = data[max][0],
-							aux2 = data[max][1];
-
-						data[max][0] = data[top-1][0];
-						data[max][1] = data[top-1][1];
-
-						data[top-1][0] = aux1;	
-						data[top-1][1] = aux2;
-
-						max = 0;
+			//Variables auxiliares para agua, población y datos añadidos.
+			int water = 0, citizen = 0, added = 1;			
+			int i = 0;
+			for (i = 0; i < cases; i++){
+				//datos auxiliares para promedio, personas y agua.
+				int avg = 0, people = 0, agua = 0;
+				//Lectura de datos.
+				if(scanf("%i %i", &people, &agua) == -1);
+				//Sumatoria de personas y agua totales.
+				citizen += people;
+				water += agua;
+				//Promedios.
+				avg = (int) floor(agua / people);
+				//Primer caso
+				if(i == 0){
+					data[0][0]= people;
+					data[0][1]= avg;
+				//Caso donde el dato que entra es, en promedio, mayor a resto.
+				}else if(data[added-1][1] < avg){
+					data[added][0] = people;
+					data[added][1] = avg;
+					added++;
+				//Se busca la posición del nuevo dato.
+				}else{
+					int j;
+					for(j = 0; j < i; j++){
+						//Caso donde el promedio es igual.
+						if(data[j][1] == avg){
+							data[j][0] += people;
+							break;
+						//Caso donde el promedio queda justo en medio de los datos.
+						}else if(data[j][1] > avg){
+							int k;
+							//Se abre el espacio para el nuevo dato.
+							for(k = i+1; k >= j+1; k--){
+								data[k][0] = data[k-1][0];
+								data[k][1] = data[k-1][1];
+							}
+							//Se incluye en nuevo dato. 
+							data[j][0] = people;
+							data[j][1] = avg;
+							added++;
+							break;
+						}
 					}
 				}
 			}
-			//Termina buble sort
-
-			//Imprimo los resultados
+			//Impresión de datos.
 			printf("Cidade# %i:\n", ++cities);
-			for (int i = 0; i < cases; i++){
-				printf("%i-%i ", data[i][0], (int) floor(data[i][1]/data[i][0]));
+			for (i = 0; i < added; i++){
+				printf("%i-%i ", data[i][0], data[i][1]);
 			}
-			printf("\nConsumo medio: %0.2f m3.\n",(float) water/citizen);
+			printf("\nConsumo medio: %.2f m3.\n",trunc(((float)water/citizen)*100)/100);
 		}else break;//Rompo el ciclo while. 
 	}while(1);
 }
